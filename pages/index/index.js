59d7bf182051843c4d86a259bf3cb533a7a22e82
CHP <h1016821721@163.com>
2022-10-23 19:45:10 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,118 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/index/index.js
+import request from '../../utils/request'
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    bannerList:[],//轮播图数据
+    recommendList:[],//推荐歌单数据
+    topList:[],//排行榜数据
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: async function (options) {
+    //滑块数据
+    let bannerListData = await request('/banner', {type:2});
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      bannerList: bannerListData.banners
     })
+    //推荐歌单数据
+    let recommendListData = await request('/personalized', {limit:10})
+    this.setData({
+      recommendList: recommendListData.result
+    })
+    // 排行榜数据
+    let index = 0;
+    let resultArr = [{"name": "飙升榜",
+    "id": 19723756,},{"name": "新歌榜",
+    "id": 3779629,},{"name": "原创榜",
+    "id": 2884035,},{"name": "热歌榜",
+			"id": 3778678}];
+      resultArr.forEach(async(item)=>{
+        let topListData = await request('/playlist/detail', {id:item.id});
+        // console.log(topListData)
+          item.tracks=topListData.playlist.tracks.slice(0, 5)
+      this.setData({
+        topList: resultArr
+      })
+      })
+
+  },
+  //跳转到每日推荐歌曲页面
+  toRecommendSong(){
+    wx.navigateTo({
+      url: '/pages/recommendSong/recommendSong',
+    })
+  },
+  //跳转到搜索页面
+  toSearch(){
+    wx.navigateTo({
+      url: '/pages/search/search',
+    })
+  },
+
+  toSongDetail(event){
+    wx.navigateTo({
+      url: '/pages/songDetail/songDetail?song=' + event.currentTarget.id
+    })
+  },
+  //跳转到歌单歌曲列表页面
+  toPlayList(event){
+    wx.navigateTo({
+      url: '/pages/playlist/playlist?id=' + event.currentTarget.id
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
